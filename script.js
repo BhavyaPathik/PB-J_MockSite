e769b83ab4fc0adfad2f118592f0fd5692225abd
Bhavya_Pathik <bhavvy2006@gmail.com>
2025-08-24 18:33:08 +0530

    Resolved merge conflicts and cleaned code

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
 document.getElementById("scroll-top").addEventListener("click", function(){
     window.scrollTo({top: 0, behavior: 'smooth'});
 });
-=======
-document.getElementById("scroll-top").addEventListener("click", function(){
-    window.scrollTo({top: 0, behavior: 'smooth'});
-});
->>>>>>> 8e51aa42eae6e8e20a7edbb26e838213af054239
 // scroll wheel working in js
